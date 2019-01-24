1204570e6919432554a31627d1673ec1a99d4e22
Kent Butler <KentDavidButler@gmail.com>
2019-01-24 09:58:09 -0500

    initial commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 30;
+            for (int i = 0; i <= x; i++)
+            {
+                FizzBuzz(i);
+                Console.Write(" ");
+            }
+
+        }
+
+        private static void Fizz(int x)
+        {
+            if((x%3) == 0)
+            {
+                Console.Write("Fizz ");
+            }
+
+        }
+
+        private static void Buzz(int x)
+        {
+            if((x%5) == 0)
+            {
+                Console.Write("Buzz ");
+            }
+
+        }
+
+        private static void FizzBuzz(int x)
+        {
+            Fizz(x);
+            Buzz(x);
+            Console.WriteLine(":Next. ");
+        }
+    }
+}
